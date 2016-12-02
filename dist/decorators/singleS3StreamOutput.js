54396858614b359ba567df30938008efc340d5bc
Ryan Palmer <tog54321@gmail.com>
2016-12-01 23:24:33 -0500

    update

--- dist/decorators/singleS3StreamOutput.js
+++ dist/decorators/singleS3StreamOutput.js
@@ -116,7 +116,7 @@ function singleS3StreamOutput() {
                                         Bucket: process.env.AWS_S3_TEMP_BUCKET,
                                         ContentType: 'application/zip',
                                         ContentEncoding: 'chunked',
-                                        Key: _uuid2.default.v4(),
+                                        Key: _uuid2.default.v4() + '.zip',
                                         Body: readStream.pipe(new _stream.PassThrough())
                                     };
                                     s3Options = {
